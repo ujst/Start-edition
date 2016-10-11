19f73264706613102126f20c497bdda5735a8421
ujst <ujst@yandex.ua>
2016-10-12 02:59:44 +0300

    Update UsingMethods.java
    Упрощение кода с помощью цепочки if - else.

--- UsingMethods.java
+++ UsingMethods.java
@@ -3,19 +3,16 @@ class Methods {
     String textFizz = "Fizz";
     String textBuzz = "Buzz";
 
-    void Counter(int i) {
-        System.out.print(i + " ");
-    }
-
-    void OutFizzBuzz(int i) {
+    void outFizzOutBuzz(int i) {
         int j = i;
-
-        if (j%3 == 0)
-            System.out.print(textFizz + " ");
-        if (j%5 == 0)
-            System.out.print(textBuzz + " ");
-        if (j%15 == 0)
-            System.out.print(textFizz + textBuzz + " ");
+        if (i%15 == 0)
+            System.out.println(textFizz + textBuzz);
+        else if (j%3 == 0)
+            System.out.println(textFizz);
+        else if (j%5 == 0)
+            System.out.println(textBuzz);
+        else
+            System.out.print(i + " ");
     }
 }
 
@@ -25,11 +22,8 @@ public class UsingMethods {
         Methods textOutOf = new Methods();
 
         for (int i = 1; i < 101; i++) {
+            textOutOf.outFizzOutBuzz(i);
 
-            if ((i%3 == 0) || (i%5 == 0) || (i%15 == 0))
-                textOutOf.OutFizzBuzz(i);
-            else
-                textOutOf.Counter(i);
         }
     }
 }
